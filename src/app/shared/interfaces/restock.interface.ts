7eb6be7a774aa122053bb6be26f4d49f2e1e3353
Seiji Villafranca <seiji.ralphvillafranca@kpn.com>
2024-03-31 20:24:52 +0200

    Add restock feature

--- src/app/shared/interfaces/restock.interface.ts
+++ src/app/shared/interfaces/restock.interface.ts
@@ -3,7 +3,9 @@ import { Product } from "./product.interface";
 export interface Restock {
   key: string;
   id: string;
-  products: Product[];
+  date: string;
+  products: { code: string, design: string, size: string, availableStocks: number, addStock: number, regPricePerBundle: number, qtyPerBundle: number; }[];
   usedDiscount: string;
   totalPrice: number;
+  status: 'Pending' | 'Confirmed'
 }
