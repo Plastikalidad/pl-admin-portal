77ccf9c21ee4453ba867f2c4585f5a6009e7a460
Seiji Villafranca <seiji.ralphvillafranca@kpn.com>
2024-04-14 10:02:03 +0200

    added paid status for order

--- src/app/shared/interfaces/order.interface.ts
+++ src/app/shared/interfaces/order.interface.ts
@@ -4,6 +4,7 @@ export interface Order {
   key: string;
   code: string;
   date: string;
+  paidStatus: 'Not Paid' | 'Paid',
   customer: Customer;
   products: { key: string, code: string, design: string, size: string, availableStocks: number, quantity: number, regPricePerBundle: number, qtyPerBundle: number; }[];
   totalPrice: number;
